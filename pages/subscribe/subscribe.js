eed52950f06346318a879445364de9b07a2ac1fd
miku40 <625099302@qq.com>
2017-12-14 20:03:04 +0800

    新增订阅

---
+++ pages/subscribe/subscribe.js
@@ -0,0 +1,70 @@
+// pages/subscribe/subscribe.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    subscribe:[
+      { id: 1, title: "寻梦环游记", image:"https://img1.doubanio.com/view/photo/s_ratio_poster/public/p2503997609.jpg"},
+      { id: 2, title: "寻梦环游记", image: "https://img1.doubanio.com/view/photo/s_ratio_poster/public/p2503997609.jpg" },
+      { id: 2, title: "寻梦环游记", image: "https://img1.doubanio.com/view/photo/s_ratio_poster/public/p2503997609.jpg" }
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
